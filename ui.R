da28aad7bdd9cfe73592598ee29226fde309dad8
gitreset <gitreset@gmail.com>
2015-09-27 09:22:52 -0700

    put some finishing touches on app & presentation.

--- ui.R
+++ ui.R
@@ -4,7 +4,7 @@ library(jsonlite)
 #
 shinyUI(fluidPage(
     
-    titlePanel("BMI Calculator - A Course Project for \"Developing Data Products\""),
+    titlePanel("Easy BMI Explorer - A Course Project for \"Developing Data Products\""),
     
     sidebarLayout(
         sidebarPanel(
@@ -16,19 +16,19 @@ shinyUI(fluidPage(
         
         mainPanel(
             tabsetPanel(
-                tabPanel("Calculator App",
+                tabPanel("BMI Explorer Results",
                         h3("Your Body Mass Index (BMI) is:"),
                         verbatimTextOutput("bmi"),
                         h3("Interpretation:"),
-                        h4("Underweight if BMI <18.5"),
-                        h4("Normal weight if BMI in range 18.5-24.9"),
-                        h4("Overweight if BMI in range 25-29.9"),
-                        h4("Obese if BMI of 30 or greater")
+                        h5("Underweight if BMI <18.5"),
+                        h5("Normal weight if BMI in range 18.5-24.9"),
+                        h5("Overweight if BMI in range 25-29.9"),
+                        h5("Obese if BMI of 30 or greater")
                     ),
                 tabPanel("Documentation",
-                         h3("Body Mass Index (BMI) Calculator"),
-                         p("This is a Shiny application for my Coursera course project for Developing Data Products. It is a simple calculator using height and weight information to determine a user's body mass index."),
-                         h3("Using the Calculator:"),
+                         h3("Easy BMI Explorer"),
+                         p("This is a Shiny application for my Coursera course project for Developing Data Products. It is an interactive calculator using height and weight information to determine a user's body mass index."),
+                         h3("Using the Explorer:"),
                          p("Using this app requires user input for body weight and height. To find your BMI, use the sliders to set the height and weight values. Then click the \"Calculate BMI\" button."),
                          p("The range of height and weight values allowed for this calculator is limited to exclude the tails of the weight/height distribution for a typical population. We do not include small children with height < 24 inches for the BMI calculator. On the other end of the distribution, the height and weight values have upper bounds of 350 lb and 90 inches (7/f ft).")
                 )
